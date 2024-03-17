05482ffa5f1dd23569aa2f36db9f69bbec5ba7ef
Max <maxzhmach@mail.ru>
2024-03-17 23:57:06 +0300

    [master] - Add simple message interaction

--- main.py
+++ main.py
@@ -10,6 +10,20 @@ print('Bot started...')
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.send_message(message.chat.id, 'Привет, ты написал мне /start')
+    bot.send_message(message.chat.id, f'Привет {message.chat.first_name}')
+
+@bot.message_handler(content_types=['text'])
+def send_text(message):
+    if message.text.lower() == 'привет':
+        bot.send_message(message.chat.id, 'Привет, мой создатель')
+    elif message.text.lower() == 'пока':
+        bot.send_message(message.chat.id, 'Прощай, создатель')
+    else:
+        bot.send_message(message.chat.id, 'Я тебя не понимаю')
+
+@bot.message_handler(content_types=['photo'])
+def send_photo(message):
+    bot.send_message(message.chat.id, 'Крутая фотка')
+
 
 bot.polling(none_stop=True)
